8cd53a021e6d4594dd3baaf12a67baa0a8ec03be
Thomas De Neve <thomas.deneve@student.hogent.be>
2021-11-27 17:55:27 +0100

    Auth0 login and UI

--- app/src/main/java/com/example/hier/MainActivity.kt
+++ app/src/main/java/com/example/hier/MainActivity.kt
@@ -5,27 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.auth0.android.Auth0
-import com.auth0.android.authentication.AuthenticationException
-import com.auth0.android.callback.Callback
-import com.auth0.android.provider.WebAuthProvider
 import com.auth0.android.result.Credentials
+import com.auth0.android.result.UserProfile
 import com.example.hier.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
     lateinit var binding: ActivityMainBinding
+    var cachedCredentials: Credentials? = null
+    var cachedUserProfile: UserProfile? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         //supportActionBar?.hide();
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         window.statusBarColor = ContextCompat.getColor(this, R.color.primary_pink)
@@ -36,40 +35,9 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.login_screen)
 
         setupNavigation()
-        loginWithBrowser()
     }
 
-    private fun loginWithBrowser() {
-
-        // Setup the WebAuthProvider, using the custom scheme and scope.
-        val account = Auth0(
-            "tIHBlQvkYFg35zCofxtjKPvip5Yrg2b2",
-            "devops-t4.eu.auth0.com"
-        )
-        Log.i("Test", "Print test.");
-
-        WebAuthProvider.login(account)
-            .withScheme("demo")
-            .withScope("openid profile email")
-            // Launch the authentication passing the callback where the results will be received
-            .start(this, object : Callback<Credentials, AuthenticationException> {
-                // Called when there is an authentication failure
-                override fun onFailure(exception: AuthenticationException) {
-                    // Something went wrong!
-                    Log.i("LOGIN", "Login failed")
-                }
-
-                // Called when authentication completed successfully
-                override fun onSuccess(credentials: Credentials) {
-                    // Get the access token from the credentials object.
-                    // This can be used to call APIs
-                    Log.i("LOGIN", "Login success")
-                    val accessToken = credentials.accessToken
-                }
-            })
-    }
-
-    private fun setupNavigation(){
+    fun setupNavigation(){
         /*val navController = findNavController(R.id.nav_host_fragment)
         setupActionBarWithNavController(navController)*/
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
@@ -93,4 +61,35 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+/*
+    fun loginWithBrowser() {
+        var account = Auth0(
+            "@string/auth0_clientId",
+            "@string/auth0_domain"
+        )
+
+        // Setup the WebAuthProvider, using the custom scheme and scope
+        WebAuthProvider.login(account)
+            .withScheme("demo")
+            .withScope("openid profile email")
+            // Launch the authentication passing the callback where the results will be received
+            .start(this, object : Callback<Credentials, AuthenticationException> {
+                // Called when there is an authentication failure
+                override fun onFailure(exception: AuthenticationException) {
+                    // Something went wrong!
+                    Log.i("LOGIN", exception.getDescription())
+                    Toast.makeText(context, "Login failed", Toast.LENGTH_LONG).show()
+                }
+
+                // Called when authentication completed successfully
+                override fun onSuccess(credentials: Credentials) {
+                    // Get the access token from the credentials object.
+                    // This can be used to call APIs
+                    Log.i("LOGIN", "Login success")
+                    MainActivity().cachedCredentials = credentials
+                    navigateToHome()
+                }
+            })
+    }
+ */
 }
--- app/src/main/java/com/example/hier/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/hier/ui/login/LoginFragment.kt
@@ -1,6 +1,5 @@
 package com.example.hier.ui.login
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,6 +9,12 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
+import com.example.hier.MainActivity
 import com.example.hier.R
 import com.example.hier.databinding.FragmentLoginBinding
 import com.example.hier.util.Status
@@ -17,6 +22,7 @@ import org.koin.android.ext.android.inject
 
 class LoginFragment : Fragment() {
 
+    private lateinit var account: Auth0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,11 +39,12 @@ class LoginFragment : Fragment() {
 //             Toast.makeText(context, "login successful", Toast.LENGTH_LONG).show()
 //            Log.e("test", "lalala")
 
-            var username = binding.editTextTextPersonName.text.toString()
-            var password = binding.editTextTextPassword.text.toString()
-            //TODO hash password here?
+            //var username = binding.editTextTextPersonName.text.toString()
+            //var password = binding.editTextTextPassword.text.toString()
 
-            viewModel.setCredentials(username, password)
+            //viewModel.setCredentials(username, password)
+
+            loginWithBrowser()
         }
 
         //TODO find out why this doesn't work
@@ -66,9 +73,40 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    private fun loginWithBrowser() {
+        // Setup the WebAuthProvider, using the custom scheme and scope
+        activity?.let {
+            val clientId: String = it.getString(R.string.auth0_clientId)
+            val domain: String = it.getString(R.string.auth0_domain)
+
+            account = Auth0(clientId, domain)
+
+            WebAuthProvider.login(account)
+                .withScheme("demo")
+                .withScope("openid profile email")
+                // Launch the authentication passing the callback where the results will be received
+                .start(it, object : Callback<Credentials, AuthenticationException> {
+                    // Called when there is an authentication failure
+                    override fun onFailure(error: AuthenticationException) {
+                        // Something went wrong!
+                        Log.i("LOGIN", error.getDescription())
+                        Toast.makeText(context, "Login failed", Toast.LENGTH_LONG).show()
+                    }
+
+                    // Called when authentication completed successfully
+                    override fun onSuccess(result: Credentials) {
+                        // Get the access token from the credentials object.
+                        // This can be used to call APIs
+                        Log.i("LOGIN", "Login success")
+                        MainActivity().cachedCredentials = result
+                        navigateToHome()
+                    }
+                })
+        }
+    }
+
     private fun navigateToHome(){
         val directions = LoginFragmentDirections.actionLoginFragmentToMainActivity()
         findNavController().navigate(directions)
     }
-
 }
